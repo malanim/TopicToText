2e584c9a9bafca39cc78af00c979eef7e22edfd7
malanim <max1129@mail.ru>
2024-09-20 09:24:35 +0300

    Improved error handler
    Changes to be committed:
    	modified:   main.py
    	modified:   out.py
    
    Changes not staged for commit:
    	modified:   __pycache__/init.cpython-312.pyc
    	modified:   __pycache__/out.cpython-312.pyc
    	modified:   inf.txt
    	modified:   main.py
    	modified:   out.py
    	modified:   tngc.txt

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ def get_text(theme, name, group, colledge):
         t.close()
         
         inf = 'connect!'
-        out.line(inf, 'inf')
+        out.line(inf, 'info')
         eel.say(inf)
 
         main()
@@ -25,7 +25,7 @@ def main():
         tngc = init.read_files()
         # print(tngc)
         inf = 'topic request...'
-        out.line(inf, 'inf')
+        out.line(inf, 'info')
         eel.say(inf)
         try:
                 theme = init.get_theme(tngc)
@@ -35,7 +35,7 @@ def main():
                 out.line(inf, 'error')
                 eel.say(inf)
         inf = 'plan formation...'
-        out.line(inf, 'inf')
+        out.line(inf, 'info')
         eel.say(inf)
         try:
                 pstr = init.plan_formation(theme)
@@ -45,7 +45,7 @@ def main():
                 out.line(inf, 'error')
                 eel.say(inf)
         inf = 'plan normalisation...'
-        out.line(inf, 'inf')
+        out.line(inf, 'info')
         eel.say(inf)
         try:
                 plan = init.plan_normalisation(pstr)
@@ -55,7 +55,7 @@ def main():
                 out.line(inf, 'error')
                 eel.say(inf)
         inf = 'item processing...'
-        out.line(inf, 'inf')
+        out.line(inf, 'info')
         eel.say(inf)
         try:
                 l = len(plan)
@@ -67,7 +67,7 @@ def main():
                                 data_list.append(data_item)
                                 # print(data_item)
                                 inf = 'item processing... data item: ' + str(i) + ' / ' + str(l)
-                                out.line(inf, 'inf')
+                                out.line(inf, 'info')
                                 eel.say(inf)
                         except:
                                 inf = 'nonefailed to generate item #' + str(i)
@@ -79,12 +79,12 @@ def main():
                 out.line(inf, 'error')
                 eel.say(inf)
         inf = 'creating docx file...'
-        out.line(inf, 'inf')
+        out.line(inf, 'info')
         eel.say(inf)
         try:
                 save_doc.create_file_doc(tngc, plan, data_list)
                 inf = 'continue!'
-                out.line(inf, 'inf')
+                out.line(inf, 'info')
                 eel.say(inf)
         except:
                 inf = 'failed to generate document'
--- out.py
+++ out.py
@@ -1,13 +1,20 @@
 def line(arg:str, parameter:str):
-        if (parameter==('1')or('i')or('inf')or('inform')or('information')):
-                perinf = 'INFORMATION'
-        else:
+        try:
+                ir = open('inf.txt', 'r')
+                information_number_list = [line.strip() for line in ir]
+                number = int(information_number_list[0])
+                ir.close()
+        except:
+                number = 100
+                print(str(number) + ' |ttt| ERROR: configuration file not found')
+        if (parameter=='info'):
+                perinf = 'INFO'
+        elif (parameter=='error'):
                 perinf = 'ERROR'
-        ir = open('inf.txt', 'r')
-        information_number_list = [line.strip() for line in ir]
-        number = int(information_number_list[0])
-        ir.close()
-        inf = str(number) + ' | ' + perinf + ': '
+        else:
+                perinf = 'INFO'
+                print(str(number) + ' |ttt| ERROR: configuration parameter passed incorrectly')
+        inf = str(number) + ' |ttt| ' + perinf + ': '
         print(inf + str(arg))
         iw = open('inf.txt', 'w')
         number = number + 1
